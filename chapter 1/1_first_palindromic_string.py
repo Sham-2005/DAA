aa23a9a0c13437f7ecab304a94011c9f4f5fd317
Sham-2005 <shams5076.sse@saveetha.com>
2026-06-22 22:34:57 +0530

    Delete chapter 1 directory

--- chapter 1/1_first_palindromic_string.py
+++
@@ -1,8 +0,0 @@
-def firstPalindrome(words):
-    for word in words:
-        if word == word[::-1]:
-            return word
-    return ""
-
-print(firstPalindrome(["abc","car","ada","racecar","cool"]))
-print(firstPalindrome(["notapalindrome","racecar"]))         
