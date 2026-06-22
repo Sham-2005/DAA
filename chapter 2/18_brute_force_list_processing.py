6146725b062473ee70d50db7a1f47e48dbdd37c3
Sham-2005 <shams5076.sse@saveetha.com>
2026-06-22 22:35:10 +0530

    Delete chapter 2 directory

--- chapter 2/18_brute_force_list_processing.py
+++
@@ -1,7 +0,0 @@
-def process_list(input_list):
-    return input_list
-
-print(process_list([]))                       
-print(process_list([1]))                      
-print(process_list([7, 7, 7, 7]))             
-print(process_list([-5, -1, -3, -2, -4]))     
